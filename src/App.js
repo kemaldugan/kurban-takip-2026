15ecd1b83f15fd418353f90fb6e2f1925b1c7636
kemaldugan <kemaldugan@yahoo.com>
2026-03-14 02:07:49 +0300

    Update WA_YONETICI phone number

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const kesimSaati = (sira) => {
 const zaman = () => new Date().toLocaleString("tr-TR");
 const getBos = (h) => h.maxHisse - h.hisseler.filter(x=>x.durum==="onaylı").length;
 
-const WA_YONETICI = "905321234567"; // ← kendi numaranızla değiştirin
+const WA_YONETICI = "905063163567"; // ← kendi numaranızla değiştirin
 const waGonder = (tel, mesaj) => {
   const temizTel = String(tel).replace(/\D/g,"").replace(/^0/,"90");
   window.open(`https://wa.me/${temizTel}?text=${encodeURIComponent(mesaj)}`,"_blank");
